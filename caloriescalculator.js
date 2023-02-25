cd4ab8e8671a4395c5ec913900d87899d405236b
Yurij Shulika <105563150+Pretya@users.noreply.github.com>
2023-02-25 16:02:18 +0200

    Add files via upload

---
+++ caloriescalculator.js
@@ -0,0 +1,7 @@
+function runDistance(weight, distance){
+  return weight * distance;
+}
+
+function runDistanceHeart(heartWeight, runningTime, runningDistanceTime){
+  return 0.75 * heartWeight * runningTime * runningDistanceTime;
+}
